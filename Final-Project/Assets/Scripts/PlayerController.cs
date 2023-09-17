fb8cce392a71de173cb500ff3e4cafb87e7b7c96
David Tsoenyane <davidtsoenyane@gmail.com>
2023-09-17 16:17:06 +0200

    enemy placeholder and melee attack collision detection. Adjusted Player Animations with relevant adjustments to blend tree.

--- Final-Project/Assets/Scripts/PlayerController.cs
+++ Final-Project/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float MovementSpeed;
     private Vector2 MovementInput;
     private SpriteRenderer spriteRenderer;
+    //public SwordAttack swordAttack;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -16,7 +17,7 @@ public class PlayerController : MonoBehaviour
         spriteRenderer= GetComponent<SpriteRenderer>();
     }
     // Start is called before the first frame update
-    private void OnMove(InputValue inputValue)
+    public void OnMove(InputValue inputValue)
     {
 
         Vector2 inputVector = inputValue.Get<Vector2>();
@@ -38,18 +39,71 @@ public class PlayerController : MonoBehaviour
         if (inputVector.x < 0)
         {
             // Flip the sprite to the left.
-            spriteRenderer.flipX = true;
+           // spriteRenderer.flipX = true;
+            
         }
         else if (inputVector.x > 0)
         {
             // Flip the sprite to the right.
-            spriteRenderer.flipX = false;
+           // spriteRenderer.flipX = false;
+           
         }
+
+        if (inputVector.y < 0)
+        {
+            //flip attack box up
+            
+        }
+        else if(inputVector.y > 0)
+        {
+            //flip attack box down
+          
+        }
+    }
+    public void Attack()
+    {
+        if (spriteRenderer.flipX==true)
+        {
+            // Flip the sprite to the left.
+            //swordAttack.attackleft();
+           
+        }
+        else if (spriteRenderer.flipX==false)
+        {
+            // Flip the sprite to the right.
+            //swordAttack.attackright();
+            
+        }
+
+        if (MovementInput.y < 0)
+        {
+            //flip attack box up
+            //swordAttack.attackup();
+        }
+        else if (MovementInput.y > 0)
+        {
+            //flip attack box down
+            //swordAttack.attackdown();
+        }
+    }
+    public void stopattack()
+    {
+        //swordAttack.stopattack();
     }
     private void Animate()
     {
-        anim.SetFloat("Movement_X", MovementInput.x);
-        anim.SetFloat("Movement_Y", MovementInput.y);
+        if (Mathf.Abs(MovementInput.x)>Mathf.Abs(MovementInput.y))
+        {
+            anim.SetFloat("Movement_X", MovementInput.x* (1/Mathf.Abs(MovementInput.x)));
+            anim.SetFloat("Movement_Y", 0);
+        }
+        else
+        {
+            anim.SetFloat("Movement_X", 0);
+            anim.SetFloat("Movement_Y", MovementInput.y * (1/Mathf.Abs(MovementInput.y)));  
+        }
+       // anim.SetFloat("Movement_X", MovementInput.x);
+       // anim.SetFloat("Movement_Y", MovementInput.y);
         
     }
     private void OnFire()
