6c70bc5cf621b01485dd41de4c635766858ac829
David Tsoenyane <davidtsoenyane@gmail.com>
2023-10-01 22:00:57 +0200

    tweaks to player controller and some changes to camera

--- Final-Project/Assets/Scripts/PlayerController.cs
+++ Final-Project/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,12 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     public float MovementSpeed;
-    private Vector2 MovementInput;
+    private Vector2 movementInput;
     private SpriteRenderer spriteRenderer;
-    private Vector2 pointerinput;
-    private Weaponrotate weaponrotate;
-    [SerializeField]
-    private InputActionReference Look;
-
+    public ContactFilter2D movementFilter;
+    public float collisionOffset = 0.05f;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    bool canMove = true;
 
     //public SwordAttack swordAttack;
     private void Awake()
@@ -21,27 +20,13 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         spriteRenderer= GetComponent<SpriteRenderer>();
-        weaponrotate = GetComponentInChildren<Weaponrotate>();
+        
     }
     // Start is called before the first frame update
     public void OnMove(InputValue inputValue)
     {
 
-        Vector2 inputVector = inputValue.Get<Vector2>();
-        MovementInput = inputVector;
-        if (inputVector == Vector2.zero)
-        {
-            // If the input vector is zero (no input), stop the player's movement.
-            rb.velocity = Vector2.zero;
-        }
-        else
-        {
-            // If there is input, update the player's movement based on the input.
-            rb.velocity = new Vector2(inputVector.x * MovementSpeed, inputVector.y * MovementSpeed);
-        }
-
-        // Store the input for other purposes if needed.
-      
+        movementInput = inputValue.Get<Vector2>();
 
       
     }
@@ -55,15 +40,15 @@ public class PlayerController : MonoBehaviour
     }
     private void Animate()
     {
-        if (Mathf.Abs(MovementInput.x)>Mathf.Abs(MovementInput.y))
+        if (Mathf.Abs(movementInput.x)>Mathf.Abs(movementInput.y))
         {
-            anim.SetFloat("Movement_X", MovementInput.x* (1/Mathf.Abs(MovementInput.x)));
+            anim.SetFloat("Movement_X", movementInput.x* (1/Mathf.Abs(movementInput.x)));
             anim.SetFloat("Movement_Y", 0);
         }
         else
         {
             anim.SetFloat("Movement_X", 0);
-            anim.SetFloat("Movement_Y", MovementInput.y * (1/Mathf.Abs(MovementInput.y)));  
+            anim.SetFloat("Movement_Y", movementInput.y * (1/Mathf.Abs(movementInput.y)));  
         }
        // anim.SetFloat("Movement_X", MovementInput.x);
        // anim.SetFloat("Movement_Y", MovementInput.y);
@@ -77,20 +62,68 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         Animate();
-        
-             rb.velocity = MovementInput * MovementSpeed;  
-       
-    }
-    private Vector2 GetPointerInput()
-    {
-        Vector3 mousePos = Look.action.ReadValue<Vector2>();
-        mousePos.z = Camera.main.nearClipPlane;
-        return Camera.main.ScreenToWorldPoint(mousePos);
-    }
-    private void Update()
-    {
-        pointerinput = GetPointerInput();
-        weaponrotate.Pointerposition = pointerinput;
 
-     }
+        if (canMove)
+        {
+            if (movementInput != Vector2.zero)
+            {
+                bool success = TryMove(movementInput);
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(movementInput.x, 0));
+
+
+                    if (!success)
+                    {
+                        success = TryMove(new Vector2(0, movementInput.y));
+                    }
+                }
+              
+
+            }
+         
+
+            //set direction of sprite to movement direction
+
+
+            
+        }
+
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
+            int count = rb.Cast(direction, movementFilter, castCollisions, MovementSpeed * Time.fixedDeltaTime + collisionOffset);
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direction * MovementSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+
+
+
+    }
+
+    public void LockMovement()
+    {
+        canMove = false;
+    }
+
+    public void UnlockMovemnt()
+    {
+        canMove = true;
+    }
+
 }
---
+++ Final-Project/Assets/Swordattack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swordattack : MonoBehaviour
+{
+    public void AttackRight()
+    {
+
+
+    }
+
+    public void AttackLeft()
+    {
+
+    }
+
+    public void StopAttack()
+    {
+
+    }
+}
