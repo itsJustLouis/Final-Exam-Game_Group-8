48485af8e0fd14b813458713bcda280529033b8d
Sihle-Mazibuko <sihlemazii@gmail.com>
2023-10-05 23:53:50 +0200

    Aim?

---
+++ Final-Project/Assets/Scripts/Lighting/Torch.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    Camera mainCam;
+    Vector3 mousePos;
+
+    private void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 rotation = mousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0,0,rotZ);
+    }
+
+}
