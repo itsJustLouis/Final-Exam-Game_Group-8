c19326b253f14db045ecbb1a9f7922cea3afeac7
David Tsoenyane <davidtsoenyane@gmail.com>
2023-10-12 17:15:35 +0200

    adjustments to player controller and exploding enemy

--- Final-Project/Assets/Scripts/Battery.cs
+++ Final-Project/Assets/Scripts/Battery.cs
@@ -9,18 +9,26 @@ public class Battery : MonoBehaviour
     public float regenerationDelay = 5f; // Time in seconds before regeneration starts
     public int currentAmount;
     public float regenerationTime = 5.0f; // Time in seconds to fully regenerate
+    private GameObject[] batteryUIObjects;
 
     void Start()
     {
         currentAmount = batteryLevel;
+        batteryUIObjects = GameObject.FindGameObjectsWithTag("BatteryUI");
+        for (int i = 0; i < batteryUIObjects.Length; i++)
+        {
+            if (i >= currentAmount)
+            {
+                batteryUIObjects[i].SetActive(false);
+            }
+        }
     }
 
     void Update()
     {
-        Debug.Log("Batter:"+ currentAmount);
+        Debug.Log("Battery:" + currentAmount);
         if (currentAmount < batteryLevel && !isRegenerating)
         {
-           
             StartCoroutine(Regenerate());
         }
     }
@@ -32,19 +40,32 @@ public class Battery : MonoBehaviour
         while (currentAmount < batteryLevel)
         {
             yield return new WaitForSeconds(regenerationTime / batteryLevel);
-            currentAmount += 1;
-          
+            if (currentAmount < batteryLevel)
+            {
+                currentAmount += 1;
+                batteryUIObjects[currentAmount - 1].SetActive(true);
+                Debug.Log("Battery level after regeneration: " + currentAmount);
+            }
+            else
+            {
+                break;
+            }
         }
         isRegenerating = false;
     }
-
 
     public void DecreaseAmount()
     {
         if (currentAmount > 0)
         {
+            StopCoroutine(Regenerate());
+            isRegenerating = false;
             currentAmount -= 1;
+            batteryUIObjects[currentAmount].SetActive(false);
             Debug.Log(currentAmount);
         }
     }
+
+
+
 }
--- Final-Project/Assets/Scripts/PlayerController.cs
+++ Final-Project/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
@@ -16,6 +18,7 @@ public class PlayerController : MonoBehaviour
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
     bool canMove = true;
     public float speed;
+    public float runSpeed = 7f;
     public float stamina;
     public float maxStamina;
     public bool isrunning;
@@ -23,9 +26,15 @@ public class PlayerController : MonoBehaviour
     public Light2D FlashLight;
     public Light2D ConeLight;
     public InputAction accelerateAction;
-    public Battery Battery;
+    public FlashLight BatteryLevel;
     public GameObject DmgLightCollider;
     public GameObject LightCollider;
+    public Slider StaminSlider;
+    public float staminaDecreaseRate = 2f;
+    public float staminaIncreaseRate = 1f;
+    public float staminaRefillDelay = 2f;
+    private bool isRunning = false;
+    private bool isRefillingStamina = false;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -33,11 +42,12 @@ public class PlayerController : MonoBehaviour
         spriteRenderer= GetComponent<SpriteRenderer>();
         stamina = maxStamina;
         MovementSpeedCache = MovementSpeed;
-        Battery= GetComponentInChildren<Battery>();
+        BatteryLevel= GetComponentInChildren<FlashLight>();
         DmgLightCollider.active= false;
         LightCollider.active = true;
 
-
+        StaminSlider.maxValue = stamina;
+        StaminSlider.value = stamina;
     }
     // Start is called before the first frame update
     public void OnMove(InputValue inputValue)
@@ -47,30 +57,9 @@ public class PlayerController : MonoBehaviour
 
       
     }
-    public void OnAccelerate()
-    {
-        if (stamina > 0)
-        {
-            IsRunning();
-            //MovementSpeed = 5;
-            stamina -= Time.deltaTime; // decrease over time
-           
-        }
-        else
-        {
-            // If Space is not pressed or stamina is empty, return speed to normal and gradually refill stamina
-         //   MovementSpeed = 2;
-            if (stamina < maxStamina)
-            {
-                stamina += Time.deltaTime; // increase over time
-            }
-            isrunning = false;
+   
 
-            // Ensure stamina stays within its bounds
-            stamina = Mathf.Clamp(stamina, 0, maxStamina);
-        }
-
-    }
+   
     public bool IsRunning()
     {
         
@@ -85,19 +74,7 @@ public class PlayerController : MonoBehaviour
         isrunning = false;
         return isrunning;
     }
-    public void OnAccelerateCanceled()
-    {
-        // If Space is not pressed or stamina is empty, return speed to normal and gradually refill stamina
-        MovementSpeed = 2;
-        if (stamina < maxStamina)
-        {
-            stamina += Time.deltaTime; // increase over time
-        }
-        isrunning = false;
-
-        // Ensure stamina stays within its bounds
-        stamina = Mathf.Clamp(stamina, 0, maxStamina);
-    }
+    
 
     public void Attack()
     {
@@ -125,13 +102,17 @@ public class PlayerController : MonoBehaviour
     }
     private void OnFire()
     {
-        FlashLight.color = Color.red;
-        Invoke("WhiteFlash", 0.5f);
-        Battery.DecreaseAmount();
-        Battery.isRegenerating = false;
-        DmgLightCollider.active = true;
-       canMove = false;
-        LightCollider.active = false;
+        if (BatteryLevel.currentBatteryLevel>=1)
+        {
+            FlashLight.color = Color.red;
+            Invoke("WhiteFlash", 0.5f);
+            BatteryLevel.DecreaseBatteryLevel();
+
+            DmgLightCollider.active = true;
+            canMove = false;
+            LightCollider.active = false;
+        }
+      
     }
     public void WhiteFlash()
     {
@@ -143,47 +124,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         Gamepad gamepad = Gamepad.current; //get the current gamepad
-        if ((Input.GetKey(KeyCode.Space) || (gamepad != null && gamepad.buttonEast.isPressed)) && stamina > 1)
+
+        if ((Input.GetKey(KeyCode.Space) || (gamepad != null && gamepad.buttonEast.isPressed)) && stamina > 0)
         {
-            IsRunning();
             isrunning = true;
-            MovementSpeed = 7;
-            stamina -= Time.deltaTime; // decrease over time
+           MovementSpeed = runSpeed;
+            stamina -= staminaDecreaseRate * Time.deltaTime; // decrease over time
         }
         else
         {
-            // If Space is not pressed or stamina is empty, return speed to normal and gradually refill stamina
-          MovementSpeed = MovementSpeedCache;
-            if (stamina < maxStamina)
+            isrunning = false;
+            MovementSpeed = MovementSpeedCache;
+
+            if (!isRefillingStamina && stamina < maxStamina)
             {
-                stamina += Time.deltaTime; // increase over time
+                StartCoroutine(RefillStaminaAfterDelay(staminaRefillDelay));
             }
-            //delay on running
-            Invoke("IsNotRunning", 1f);
         }
 
         // Ensure stamina stays within its bounds
         stamina = Mathf.Clamp(stamina, 0, maxStamina);
+        // Ensure stamina stays within its bounds
+        //  stamina = Mathf.Clamp(stamina, 0, maxStamina);
 
         Animate();
+        StaminSlider.value = stamina;
+        HandleFlashlight();
 
-
-        if (isrunning)
-        {
-            FlashLight.intensity = 0f;
-            ConeLight.intensity = 0f;
-            StopCoroutine("TurnOnLight");
-        }
-        else
-        {
-            // If the player is not running and the light is off, turn it back on after a delay
-            if (FlashLight.intensity == 0f && !isrunning)
-            {
-                Invoke("TurnOnLight", 3f); // Wait for 3 seconds before turning on the light
-            }
-        }
+       
         if (canMove)
         {
             if (movementInput != Vector2.zero)
@@ -216,6 +185,42 @@ public class PlayerController : MonoBehaviour
 
 
 
+    }
+    IEnumerator StopRunningAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        IsNotRunning();
+    }
+    IEnumerator RefillStaminaAfterDelay(float delay)
+    {
+        isRefillingStamina = true;
+
+        yield return new WaitForSeconds(delay);
+
+        while (!isrunning && stamina < maxStamina)
+        {
+            stamina += staminaIncreaseRate * Time.deltaTime; // increase over time
+            yield return null; // wait until next frame
+        }
+
+        isRefillingStamina = false;
+    }
+    void HandleFlashlight()
+    {
+        if (isrunning)
+        {
+            FlashLight.intensity = 0f;
+            ConeLight.intensity = 0f;
+            StopCoroutine("TurnOnLight");
+        }
+        else
+        {
+            // If the player is not running and the light is off, turn it back on after a delay
+            if (FlashLight.intensity == 0f)
+            {
+                Invoke("TurnOnLight", 3f); // Wait for 3 seconds before turning on the light
+            }
+        }
     }
     void TurnOnLight()
     {
